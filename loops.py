4e09546371564dbf053bed9ee08b85937a8cbe92
Bukenya-Elijah-Kirabo <kiraboelijah151@gmail.com>
2025-06-26 22:28:54 +0300

    Add files via upload

--- loops.py
+++ loops.py
@@ -14,3 +14,21 @@ while user_name != "admin" or passcode != "passcode":
     user_name = input()
     print("enter your passcode") 
     passcode = input()
+pass
+
+
+
+
+def main():
+    print("Welcome to the Indentation Race!")
+    for i in range(5):
+        print("Current number:", i)
+        if i % 2 == 0:
+            print("Even number.")
+        else:
+            print("Odd number.")
+    print("Race completed!")
+
+main()
+
+
