b4d87caca7979124a3361735fb9432f8d5479e0b
Bukenya-Elijah-Kirabo <kiraboelijah151@gmail.com>
2025-09-18 14:02:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// let age =15;
+
+// age = 25;
+
+// const my_name = "Bukenya";
+
+// var school = "UC";
+
+
+// let course_work_marks = 5;
+// let final_mark = 6;
+
+// const my_name = "Bukenya";
+
+// let complied_mark = course_work_marks + final_mark;
+// console.log('The final mark is: ' + complied_mark); 
+// console.log('The final mark is: ' + (course_work_marks + final_mark));
+
+const my_name = "Bukenya";
+const other_name = "Elijah";
+
+console.log(my_name + other_name);
+
+console.log(`${my_name} ${other_name}`);
